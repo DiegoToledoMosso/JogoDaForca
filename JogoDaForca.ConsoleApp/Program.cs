db2a1da8cb5ffa000d9357a2ff85ade9cde4ec8f
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-05 20:44:04 -0300

    Estrutura basica do jogo da forca

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        //Definir estrutura básica do programa e receber input do usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Jogo da Forca");
+
+            while (true)
+            {
+
+
+                Console.Clear();    
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("-------------------------------------");
+
+                Console.Write("Digite uma letra: ");
+                char chute = Console.ReadLine()[0]; // char obetém, apenas uma letra de uma palavra(caracter)
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine(); 
+            }
+
+
+
+
+            
+
         }
     }
 }
