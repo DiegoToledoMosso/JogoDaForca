8c7abc0f51906dcdf138d9b5b20d860fccb2c934
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-05 20:59:58 -0300

    Exibir palavra oculta com traços

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,17 +2,30 @@
 {
     internal class Program
     {
-        //Definir estrutura básica do programa e receber input do usuário
+        //Versão 2: Exibir palavra oculta com traços
         static void Main(string[] args)
         {
 
             while (true)
             {
 
+                string palavraSecreta = "MELANCIA";
+
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+                {
+                    //acessar o array no indicce 0
+                    letrasEncontradas[caractere] = '_';
+                }
+
+                string dicaDaPalavra = String.Join("", letrasEncontradas);
 
                 Console.Clear();    
                 Console.WriteLine("-------------------------------------");
                 Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("palavra secreta : " + palavraSecreta);
                 Console.WriteLine("-------------------------------------");
 
                 Console.Write("Digite uma letra: ");
